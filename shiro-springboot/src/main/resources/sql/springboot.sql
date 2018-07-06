20ca321547fc682a69d915ff1eb14602e3c5eaef
a982338665 <luofengli123@163.com>
2018-07-06 23:33:56 +0800

    表结构

--- shiro-springboot/src/main/resources/sql/springboot.sql
+++ shiro-springboot/src/main/resources/sql/springboot.sql
@@ -1,32 +1,98 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost         10.0.10.195
-Source Server Version : 50720
+Source Server         : localhost
+Source Server Version : 50521
 Source Host           : localhost:3306
 Source Database       : springboot
 
 Target Server Type    : MYSQL
-Target Server Version : 50720
+Target Server Version : 50521
 File Encoding         : 65001
 
-Date: 2018-07-06 18:03:31
+Date: 2018-07-06 23:32:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for `menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menucode` varchar(10) DEFAULT NULL,
+  `menuname` varchar(10) DEFAULT NULL,
+  `menupath` varchar(50) DEFAULT NULL,
+  `menuparent` tinyint(3) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `role`
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rolename` varchar(20) DEFAULT NULL,
+  `rolecode` varchar(10) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `role_menu`
+-- ----------------------------
+DROP TABLE IF EXISTS `role_menu`;
+CREATE TABLE `role_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rid` int(11) DEFAULT NULL,
+  `mid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of role_menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `pass` varchar(50) NOT NULL,
+  `perm` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'li', '123');
+INSERT INTO `user` VALUES ('1', 'li', '123', 'user:add');
+INSERT INTO `user` VALUES ('2', 'qw', '123', 'user:update');
+
+-- ----------------------------
+-- Table structure for `user_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rid` int(20) NOT NULL,
+  `uid` int(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
